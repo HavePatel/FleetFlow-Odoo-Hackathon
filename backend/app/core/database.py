fbeae473bc75bd18d2eecaea6c6cbea623e5b04c
Khushivee <vadadoriyakhushi18@gmail.com>
2026-02-21 11:57:54 +0530

    chnaged path of Database file

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,9 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://user:password@localhost/fleetflow"
+DATABASE_URL = "sqlite:///./app/fleetflow.db"
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
+
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
 Base = declarative_base()
